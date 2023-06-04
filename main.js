31356a96d549d34ab107470783d62717a6c92f0b
Mohamed atef <mohammed.atef.saif@gmail.com>
2023-06-04 21:54:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function convert() {
+    // Get input values
+    var inputValue = parseFloat(document.getElementById("inputValue").value);
+    var fromUnit = document.getElementById("fromUnit").value;
+    var toUnit = document.getElementById("toUnit").value;
+  
+    // Perform the conversion
+    var result;
+    if (fromUnit === "cm" && toUnit === "inch") {
+      result = inputValue / 2.54;
+    } else if (fromUnit === "inch" && toUnit === "cm") {
+      result = inputValue * 2.54;
+    } else if (fromUnit === "m" && toUnit === "km") {
+      result = inputValue / 1000;
+    } else if (fromUnit === "km" && toUnit === "m") {
+      result = inputValue * 1000;
+    } else if (fromUnit === "cm" && toUnit === "m") {
+      result = inputValue / 100;
+    } else if (fromUnit === "m" && toUnit === "cm") {
+      result = inputValue * 100;
+    } else if (fromUnit === "cm" && toUnit === "km") {
+      result = inputValue / 100000;
+    } else if (fromUnit === "km" && toUnit === "cm") {
+      result = inputValue * 100000;
+    } else {
+      result = inputValue;
+    }
+  
+    // Display the result
+    document.getElementById("result").innerHTML = result.toFixed(2) + " " + toUnit;
+  }
+  
